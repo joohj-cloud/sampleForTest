e10cb0dedf99684e9b7b8101e8678f494d2c33e7
주형진/배포플랫폼개발팀/CL <hjjooace@nhn.com>
2023-01-27 14:06:57 +0900

    init commit

---
+++ sampleSpringBoot/src/main/java/com/example/samplespringboot/SampleController.java
@@ -0,0 +1,29 @@
+package com.example.samplespringboot;
+
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@RestController
+@RequestMapping("/customer")
+public class SampleController {
+
+    @GetMapping
+    public Customer getCustomer(@RequestParam String name, @RequestParam String email ){
+        log.info("[INFO] name = {}, age = {}", name, email);
+        log.info("Create New Object");
+
+        Customer customer = new Customer();
+        customer.setName(name);
+        customer.setAge(100);
+        customer.setEmail(email);
+
+        log.info("[INFO] customer name = {}, age = {}", customer.getName(), customer.getEmail());
+
+        return customer;
+    }
+}
